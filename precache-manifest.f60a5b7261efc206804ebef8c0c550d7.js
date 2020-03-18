5e1efb8d86fd0ab41616c049d0e5aaf4e214a838
Jelhill <talk2jelili2000@gmail.com>
2020-03-18 21:17:56 +0100

    Updates

---
+++ precache-manifest.f60a5b7261efc206804ebef8c0c550d7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782a2d2b7a0259625b170b98653c0060",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91b56aaeae35bcffb20f",
+    "url": "/static/css/main.9fd481d9.chunk.css"
+  },
+  {
+    "revision": "de908046d792163effd1",
+    "url": "/static/js/2.a93a1f6e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a93a1f6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91b56aaeae35bcffb20f",
+    "url": "/static/js/main.1b9ae51a.chunk.js"
+  },
+  {
+    "revision": "4f8b695758f1734eb085",
+    "url": "/static/js/runtime-main.6ea05656.js"
+  },
+  {
+    "revision": "ecebe1c5f2853489bfa80ea1aa1b23fa",
+    "url": "/static/media/busquets.ecebe1c5.jpg"
+  },
+  {
+    "revision": "e3bc00ea386698779b684272f2b529bc",
+    "url": "/static/media/de-jong.e3bc00ea.jpg"
+  },
+  {
+    "revision": "9004328c172e57feea4054359dba0671",
+    "url": "/static/media/dembele.9004328c.jpg"
+  },
+  {
+    "revision": "b4e32970a52d2fa23aed415b42a4f6b1",
+    "url": "/static/media/griezman.b4e32970.jpg"
+  },
+  {
+    "revision": "ee3cf1dd0e0d3c8f988463ee5fe7c181",
+    "url": "/static/media/messi.ee3cf1dd.jpg"
+  },
+  {
+    "revision": "8e6a9251ed6e4b53bfb8318590c561e8",
+    "url": "/static/media/pique.8e6a9251.jpg"
+  },
+  {
+    "revision": "e7b2494db5b696287ccb9ef8852b049c",
+    "url": "/static/media/semedo.e7b2494d.jpg"
+  },
+  {
+    "revision": "d909db9fa055dd199e0760fed6a3b20d",
+    "url": "/static/media/suarez.d909db9f.jpg"
+  },
+  {
+    "revision": "5cf3cd45b76b3a9f2ac796a9f48bb1ee",
+    "url": "/static/media/ter-stergen.5cf3cd45.jpg"
+  },
+  {
+    "revision": "d83f1d7c01777a36d6aae5ab18335f06",
+    "url": "/static/media/vidal.d83f1d7c.jpg"
+  }
+]);
